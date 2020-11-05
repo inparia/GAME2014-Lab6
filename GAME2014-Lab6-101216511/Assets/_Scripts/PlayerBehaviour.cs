007afb01ce9a95c8ed2299a30e8d004a40a597fb
inparia <inparia@live.ca>
2020-11-05 13:14:07 -0500

    Lab6 Done

--- GAME2014-Lab6-101216511/Assets/_Scripts/PlayerBehaviour.cs
+++ GAME2014-Lab6-101216511/Assets/_Scripts/PlayerBehaviour.cs
@@ -7,12 +7,16 @@ public class PlayerBehaviour : MonoBehaviour
 {
     public Joystick joyStick;
     public float joyStickHorizontalSensitivity;
+    public float joyStickVerticalSensitivity;
     public float horizontalForce;
     public float verticalForce;
+    public bool isGrounded;
+    public bool isJumping;
 
     private Rigidbody2D m_rigidBody2D;
     private SpriteRenderer m_spriteRenderer;
     private Animator m_animator;
+    public Transform spawnPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,17 +33,57 @@ public class PlayerBehaviour : MonoBehaviour
 
     void Move()
     {
-        if(joyStick.Horizontal > joyStickHorizontalSensitivity)
+        if (isGrounded)
         {
-            m_rigidBody2D.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
-        }
-        else if (joyStick.Horizontal < -joyStickHorizontalSensitivity)
-        {
-            m_rigidBody2D.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
-        }
-        else
-        {
+            if (joyStick.Horizontal > joyStickHorizontalSensitivity)
+            {
+                m_rigidBody2D.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
+                m_spriteRenderer.flipX = false;
+                m_animator.SetInteger("AnimState", 1);
+            }
+            else if (joyStick.Horizontal < -joyStickHorizontalSensitivity)
+            {
+                m_rigidBody2D.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
+                m_spriteRenderer.flipX = true;
+                m_animator.SetInteger("AnimState", 1);
+            }
+           
 
+
+            else if (!isJumping)
+            {
+                m_animator.SetInteger("AnimState", 0);
+            }
+
+            if ((joyStick.Vertical > joyStickVerticalSensitivity) && (!isJumping))
+            {
+                m_rigidBody2D.AddForce(Vector2.up * verticalForce * Time.deltaTime);
+                m_animator.SetInteger("AnimState", 2);
+                isJumping = true;
+            }
+            else
+            {
+                isJumping = false;
+            }
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Hazard"))
+        {
+            transform.position = spawnPoint.position;
+        }
+        
     }
 }
